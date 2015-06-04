eaf092494173b67ba28e3a9bf36f021e37c6d6a1
icbat <cobb.evan@gmail.com>
2015-06-03 19:12:58 -0500

    data comes back and is starting to get parsed

--- src/datasource.py
+++ src/datasource.py
@@ -1,5 +1,15 @@
 from os import environ
+import requests
 
 print "Looking for environment variable MATCH_DATA for data store..."
-DATA_STORE = environ['MATCH_DATA']
+DATA_STORE_URL = environ['MATCH_DATA']
 print "found match data location!"
+
+def get_match_slips():
+    page = requests.get(DATA_STORE_URL)
+    html = page.content
+    html = html.split("<tbody>")[1]
+    html = html.split("</tbody>")[0]
+    print html
+
+get_match_slips()
