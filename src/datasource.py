695c1c79893f3eb3a189f3db9c7ca18b16bc59d8
icbat <cobb.evan@gmail.com>
2015-06-03 19:56:41 -0500

    PEP-8 formatting

--- src/datasource.py
+++ src/datasource.py
@@ -11,24 +11,26 @@ def parse_match_slips(raw_match_slips):
     slips = []
     for slip in raw_match_slips:
         fields = slip.split("</td>")
-        slipFields = []
+        slip_fields = []
         for field in fields:
             parsed = field.split(">")[-1]
             if len(parsed) > 0:
-                slipFields.append(parsed)
+                slip_fields.append(parsed)
 
-        if len(slipFields) == 5:
-            reporter = slipFields[1]
-            wins = slipFields[2]
-            opponent = slipFields[3]
-            losses= slipFields[4]
+        if len(slip_fields) == 5:
+            reporter = slip_fields[1]
+            wins = slip_fields[2]
+            opponent = slip_fields[3]
+            losses = slip_fields[4]
             slips.append(MatchRecord(reporter, wins, opponent, losses))
 
     return slips
 
+
 def get_match_slips():
     raw_match_slips = get_raw_match_slips()
     return parse_match_slips(raw_match_slips)
+
 
 def get_raw_match_slips():
     page = requests.get(DATA_STORE_URL)
--- src/standings.py
+++ src/standings.py
@@ -1,57 +1,61 @@
 import datasource
 
-def get():
-    matchSlips = datasource.get_match_slips()
-    return calculatePoints(matchSlips)
 
-"""takes a list of MatchRecords"""
-def calculatePoints(matchSlips):
-    records = setupEmptyStandings(matchSlips)
-    flattenMatches(matchSlips, records)
+def get():
+    match_slips = datasource.get_match_slips()
+    return calculate_points(match_slips)
+
+
+def calculate_points(match_slips):
+    """takes a list of MatchRecords"""
+    records = setup_empty_standings(match_slips)
+    flatten_matches(match_slips, records)
     standings = {}
     for player in records:
         points = 0
 
         opponents = set([])
         for match in records[player]:
-            points = points + (3 if match.won else 1)
+            points += 3 if match.won else 1
             opponents.add(match.opponent)
-        points = points + (2 * len(opponents))
+        points += 2 * len(opponents)
         standings[player] = points
-
 
     return standings
 
-def setupEmptyStandings(matchSlips):
+
+def setup_empty_standings(match_slips):
     standings = {}
 
-    for match in matchSlips:
+    for match in match_slips:
         standings[match.reporter] = []
         standings[match.opponent] = []
     return standings
 
-def flattenMatches(matchSlips, standings):
-    for match in matchSlips:
+
+def flatten_matches(match_slips, standings):
+    for match in match_slips:
         standings[match.reporter].append(Match(match.reporter, match))
         standings[match.opponent].append(Match(match.opponent, match))
 
-def wonMatch(name, match):
+
+def won_match(name, match):
     wins = 0
     losses = 0
-    if (match.reporter == name):
+    if match.reporter == name:
         wins = match.wins
         losses = match.losses
-    elif (match.opponent == name):
+    elif match.opponent == name:
         wins = match.losses
         losses = match.wins
 
     return wins > losses
 
 
-
 class Match:
     def __init__(self, name, match):
-        self.won = wonMatch(name, match)
+        self.won = won_match(name, match)
         self.opponent = match.opponent if match.reporter == name else match.reporter
+
     def __str__(self):
         return str(self.won) + " against " + self.opponent
