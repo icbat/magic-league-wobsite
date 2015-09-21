048752a954baf95e0be96ef4695ee61defd728b1
Evan Cobb <cobb.evan@gmail.com>
2015-09-20 20:39:06 -0500

    try-except around environment variable
    so unit tests don't explode because of this

--- src/datasource.py
+++ src/datasource.py
@@ -3,8 +3,11 @@ import requests
 from match import MatchRecord
 
 print "Looking for environment variable MATCH_DATA for data store..."
-DATA_STORE_URL = environ['MATCH_DATA']
-print "found match data location!"
+try:
+    DATA_STORE_URL = environ['MATCH_DATA']
+    print "found match data location!"
+except:
+    print "WARNING! Failed to find a MATCH_DATA URL! Will not be able to get data!"
 
 
 def parse_match_slips(raw_match_slips):
